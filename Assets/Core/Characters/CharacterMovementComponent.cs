82c937341ee510dc46c9fbb7ff004e4be4fa6f43
ramonmeza <ramon_meza@live.com>
2017-11-29 11:51:09 -0500

    Initial commit

---
+++ Assets/Core/Characters/CharacterMovementComponent.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// Controls movement for a character.
+/// </summary>
+/// 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementComponent : MonoBehaviour
+{
+	/// <summary>
+	/// Speed that the character will move at
+	/// </summary>
+	public float Speed = 10.0f;
+
+	/// <summary>
+	/// Moves the game object toward world up vector.
+	/// </summary>
+	/// <param name="value">Factor to move the game object by. 
+	/// If negative, the game object will move world down.</param>
+	public void MoveUp(float value)
+	{
+		transform.position += Vector3.up * value * Speed * Time.deltaTime;
+	}
+
+	/// <summary>
+	/// Moves the game object toward world right vector.
+	/// </summary>
+	/// <param name="value">Factor to move the game object by. 
+	/// If negative, the game object will move world left.</param>
+	public void MoveRight(float value)
+	{
+		transform.position += Vector3.right * value * Speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Core/Controllers/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿/// <summary>
+/// Dispatches player controller input to the game object.
+/// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private CharacterMovementComponent m_CharacterMovementComponent;
+
+	void Start()
+	{
+		// Get the CharacterMovementComponent from the character
+		m_CharacterMovementComponent = 
+			gameObject.GetComponent<CharacterMovementComponent>();
+	}
+
+	void Update()
+	{
+		HandleInput();
+	}
+
+	/// <summary>
+	/// Handles the input events from the player.
+	/// </summary>
+	void HandleInput()
+	{
+		MoveUp( Input.GetAxisRaw( "MoveUp" ) );
+		MoveRight( Input.GetAxisRaw( "MoveRight" ) );
+	}
+
+	/// <summary>
+	/// Moves the player's character up/down using it's 
+	/// CharacterMovementComponent.
+	/// </summary>
+	void MoveUp(float value)
+	{	
+		// Check to see if there is a CharacterMovementComponent to use
+		if( m_CharacterMovementComponent != null )
+			m_CharacterMovementComponent.MoveUp( value );
+	}
+
+	/// <summary>
+	/// Moves the player's character right/left using it's 
+	/// CharacterMovementComponent.
+	/// </summary>
+	void MoveRight(float value)
+	{
+		// Check to see if there is a CharacterMovementComponent to use
+		if(m_CharacterMovementComponent != null)
+			m_CharacterMovementComponent.MoveRight( value );
+	}
+}
